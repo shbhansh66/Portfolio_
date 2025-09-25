9bff5bcd7d716a66de493091fc28fbda15592e8a
Shubhanshu <146934759+shbhansh66@users.noreply.github.com>
2025-09-25 23:58:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@
 function setDarkandLight(){
   if(window.matchMedia("(prefers-color-scheme: dark)").matches){
     document.body.classList.add("bg-gray-900","text-white");
-    document.body.classList.remove("bg-white");
+    document.body.classList.remove("bg-white","text-black");
       moon.classList.add("hidden");  
     sun.classList.remove("hidden"); 
   }
   else{
-    document.body.classList.add("bg-white" ,"text-white");
-    document.body.classList.remove("bg-gray-900");
+    document.body.classList.add("bg-white" ,"text-black");
+    document.body.classList.remove("bg-gray-900","text-white");
    
     moon.classList.remove("hidden"); 
      sun.classList.add("hidden");
